436161a2ff157c1b411017c029f495cfdf8eb91b
Log91 <>
2018-03-15 17:16:02 +0900

    클로저를 이용하여 구현 방식 변경
    전역변수를 사용해서 함수 실행 횟수를 체크하던 방식을
    클로저 스코프를 이용하여 getArea함수의 리턴 방식을 함수 리턴으로 변경했다
    함수 이름을 누적하는 배열도 같은 방식으로 처리했다
    밖에서 getArea()로 호출해야 하기에 스코프 안의 getArea()를 getPolygon으로 변경했다
    동시에 countCallFunc 변수도 fnCount로 바꿨다
    
    아직 해결하지 못한 것은 값이 누적될 때마다 console에 배열을 찍어주는데 마지막에만
    호출시켜주는 방법이다

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//공식 사용하기: 넓이 = π x 반지름^2 (넓이=파이 x 반지름 제곱)
-
-let logCallFunc = []; // 함수 호출 로그 배열
-let countCallFunc = 1; // 함수 호출 횟수 누적
+// 클로저를 이용하여 값을 카운트하던 전역변수와 실행된 함수의 이름을
+// 담아주던 전역배열변수를 없앴다.
+// 그리고 getPolygon이라는 함수의 클로저 스코프에 카운트 변수와 누적 배열 변수를 담아서 체크한다.
 
 // 원의 넓이 구하기
 function calculateCircle(radius, radius2) {
@@ -10,18 +9,12 @@ function calculateCircle(radius, radius2) {
         if (!isNumber(radius)) return '숫자타입만 계산이 가능합니다';
         if (!isPositiveNumber(radius)) return '반지름은 0보다 커야 합니다';
         result = Math.round(radius * radius * Math.PI);
-        logCallFunc.push('circle');
-        countCallFunc++;
         return '원의 넓이 : ' + result;
     } else {
         let resultArr = [];
         if (!isNumber(radius, radius2)) return '숫자타입만 계산이 가능합니다';
         if (radius > radius2) return '시작 값이 종료 값보다 큽니다';
-        for (var i = radius; i <= radius2; i++) {
-            resultArr.push(Math.round(i * i * Math.PI));
-        }
-        logCallFunc.push('circle');
-        countCallFunc++;
+        for (var i = radius; i <= radius2; i++) {}
         return '원의 넓이 : ' + resultArr;
     }
 }
@@ -31,9 +24,7 @@ function calculateRect(width, height) {
     if (isUndefined(width, height)) return '밑변과 길이 둘 다 입력해야 합니다'
     if (!isNumber(width, height)) return '숫자형 타입만 계산이 가능합니다.'
     if (!isPositiveNumber(width, height)) return '밑변과 길이는 0보다 커야 합니다';
-    logCallFunc.push('rect');
     let result = width * height;
-    countCallFunc++;
     return '사각형의 넓이 : ' + result;
 }
 
@@ -42,9 +33,7 @@ function calculateTrapezoid(top, bottom, height) {
     if (isUndefined(top, bottom, height)) return '3개의 인자가 필요합니다'
     if (!isNumber(top, bottom, height)) return '숫자형 타입만 계산이 가능합니다.'
     if (!isPositiveNumber(top, bottom, height)) return '윗변, 아랫변, 높이는 0보다 커야 합니다';
-    logCallFunc.push('trape');
     let result = (top + bottom) * height / 2
-    countCallFunc++;
     return '사다리꼴의 넓이 : ' + result;
 }
 
@@ -75,31 +64,37 @@ function isUndefined() {
 }
 
 // 다각형 넓이 구하기
-function getArea(polygon, ...args) {
-    if (polygon !== 'circle' && polygon !== 'rect' && polygon !== 'trapezoid') return '다각형값을 다시 입력하세요.'
-    if (args.length < 1) return '최소 한 가지 값이 필요합니다'
-    console.log('계산이 ' + countCallFunc + '번 일어났습니다.');
-    switch (polygon) {
-        case 'circle':
-            return calculateCircle(args[0], args[1]);
-            break;
-        case 'rect':
-            return calculateRect(args[0], args[1]);
-            break;
-        case 'trapezoid':
-            return calculateTrapezoid(args[0], args[1], args[2])
-            break;
+function getPolygon() {
+    var fnCount = 0;
+    let logCallFunc = []; // 함수 호출 로그 배열
+    function cal(polygon, ...args) {
+        if (polygon !== 'circle' && polygon !== 'rect' && polygon !== 'trapezoid') return '다각형값을 다시 입력하세요.'
+        if (args.length < 1) return '최소 한 가지 값이 필요합니다'
+        fnCount++;
+        console.log('계산이 ' + fnCount + '번 일어났습니다.');
+        logCallFunc.push(polygon);
+        console.log(logCallFunc);
+        switch (polygon) {
+            case 'circle':
+                return calculateCircle(args[0], args[1]);
+                break;
+            case 'rect':
+                return calculateRect(args[0], args[1]);
+                break;
+            case 'trapezoid':
+                return calculateTrapezoid(args[0], args[1], args[2])
+                break;
+        }
+
     }
+    return cal;
 }
 
-// 함수 호출 로그
-function getReport() {
-    console.log();
-    console.log('함수 호출 로그 : ' + logCallFunc);
-}
+
+var getArea = getPolygon();
+
 console.log(getArea('circle', 1, 20));
 console.log(getArea('circle', 3));
 console.log(getArea('circle', 2));
 console.log(getArea('rect', 2, 4));
 console.log(getArea('trapezoid', 1, 4, 2));
-getReport();
